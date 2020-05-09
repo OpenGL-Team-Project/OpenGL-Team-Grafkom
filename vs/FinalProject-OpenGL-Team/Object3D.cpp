5e1ab47f41c4f6ef69ed8da2f906b2c4d5b3ce51
grandheader <36828904+grandheader@users.noreply.github.com>
2020-05-09 20:14:21 +0700

    Update ApplyTexture at Object3D.cpp
    Updated ApplyTexture at Object3D.cpp

--- vs/FinalProject-OpenGL-Team/Object3D.cpp
+++ vs/FinalProject-OpenGL-Team/Object3D.cpp
@@ -117,16 +117,19 @@ void Object3D::ApplyTexture(const char* _texturePath) {
 		// Load and create a texture 
 		glGenTextures(1, &textureID);
 		glBindTexture(GL_TEXTURE_2D, textureID);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		int width, height;
 		unsigned char* image = SOIL_load_image(_texturePath, &width, &height, 0, SOIL_LOAD_RGBA);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
 		SOIL_free_image_data(image);
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 	else {
-		std::cout << "This ibject is not using Texture when build. \n" << std::endl;
+		std::cout << "This object is not using Texture when build. \n" << std::endl;
 	}
 }
 
